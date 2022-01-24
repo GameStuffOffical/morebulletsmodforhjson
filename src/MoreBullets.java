4610bfcae12e0805a84e74e5afa5b18ff7923a23
uujuju <84468589+uujuju1@users.noreply.github.com>
2022-01-24 19:29:54 -0300

    Update and rename src/example/bullets.java to src/MoreBullets.java

---
+++ src/MoreBullets.java
@@ -0,0 +1,19 @@
+package example;
+import mindustry.entities.bullet.*;
+import mindsutry.ctype.ContentList;
+public class MoreBullets Implements ContentList {
+	public BulletType standardHomingFast;
+	@Override
+	public void load(){
+		standardHomingFast = new BasicBulletType(6f, 12, "bullet"){{
+			width = 7f;
+			height = 9f;
+			homingPower = 0.08f;
+			reloadMultiplier = 1.5f;
+			ammoMultiplier = 5;
+			lifetime = 60f;
+		}};
+	}
+}
+
+
